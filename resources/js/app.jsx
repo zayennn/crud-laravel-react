2484d464dfbf3d040e261634e3d2171070b16469
zayennn <zaayeennn@gmail.com>
2025-08-29 01:23:32 +0700

    updated fe

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,12 +1,21 @@
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import '../css/app.css'
 
-import Table from './components/Table'
+import Home from './pages/Index'    
+import Create from './pages/Create'
+import Edit from './pages/Edit'
 
 function App() {
     return (
         <>
-            <Table />
+            <Router>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/create' element={<Create />} />
+                    <Route path='/edit/:id' element={<Edit />} />
+                </Routes>
+            </Router>
         </>
     )
 }
