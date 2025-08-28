4487add403568b060bd435494d9a71a02ea92d96
zayennn <zaayeennn@gmail.com>
2025-08-29 00:44:52 +0700

    add data mahasiswa

--- resources/js/datas/Mahasiswa.js
+++ resources/js/datas/Mahasiswa.js
@@ -1,10 +1,10 @@
 const Mahasiswa = [
-    // {id: 1, nama: 'iqbal', umur: '20', email: 'iqbal@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
-    // {id: 2, nama: 'kemal', umur: '21', email: 'kemal@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
-    // {id: 3, nama: 'dani', umur: '22', email: 'dani@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
-    // {id: 4, nama: 'vio', umur: '22', email: 'vio@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
-    // {id: 5, nama: 'kayla', umur: '23', email: 'kayla@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
-    // {id: 6, nama: 'catherine', umur: '24', email: 'catherine@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'}
+    {id: 1, nama: 'iqbal', umur: '20', email: 'iqbal@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
+    {id: 2, nama: 'kemal', umur: '21', email: 'kemal@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
+    {id: 3, nama: 'dani', umur: '22', email: 'dani@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
+    {id: 4, nama: 'vio', umur: '22', email: 'vio@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
+    {id: 5, nama: 'kayla', umur: '23', email: 'kayla@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'},
+    {id: 6, nama: 'catherine', umur: '24', email: 'catherine@example.com', no_hp: '081234567890', alamat: 'jl. example no. 123'}
 ]
 
 export default Mahasiswa
